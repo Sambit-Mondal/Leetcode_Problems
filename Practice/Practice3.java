a6f9ab8d5786167cdfd9d666ec6994e570cae472
Sambit Mondal <sambitmondal2005@gmail.com>
2024-06-21 22:02:47 +0530

    Create Practice3.java

---
+++ Practice/Practice3.java
@@ -0,0 +1,16 @@
+// Q: Print Numbers from 1 to 5 (Recursion)
+
+public class Practice3 {
+    public static void printNum (int n) {
+        if(n == 6) {
+            return;
+        }
+        System.out.println(n);
+        printNum(n + 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 1;
+        printNum(n);
+    }
+}
