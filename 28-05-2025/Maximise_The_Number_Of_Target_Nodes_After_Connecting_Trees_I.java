b3511e7b48dcd07fb9bdea3933e98591ba08a184
Sambit Mondal <sambitmondal2005@gmail.com>
2025-05-28 19:28:08 +0530

    3372. Maximise The Number Of Target Nodes After Connecting Trees I (POTD)

---
+++ 28-05-2025/Maximise_The_Number_Of_Target_Nodes_After_Connecting_Trees_I.java
@@ -0,0 +1,54 @@
+// 3372. Maximise The Number Of Target Nodes After Connecting Trees I (https://leetcode.com/problems/maximise-the-number-of-target-nodes-after-connecting-trees-i/)
+
+import java.util.*;
+
+class Maximise_The_Number_Of_Target_Nodes_After_Connecting_Trees_I {
+
+    public int[] maxTargetNodes(int[][] edges1, int[][] edges2, int k) {
+        int n = edges1.length + 1;
+        int[] count1 = build(edges1, k);
+        int[] count2 = build(edges2, k - 1);
+        int maxCount2 = 0;
+        for (int c : count2) {
+            if (c > maxCount2) {
+                maxCount2 = c;
+            }
+        }
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = count1[i] + maxCount2;
+        }
+        return res;
+    }
+
+    private int[] build(int[][] edges, int k) {
+        int n = edges.length + 1;
+        List<List<Integer>> children = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            children.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            children.get(edge[0]).add(edge[1]);
+            children.get(edge[1]).add(edge[0]);
+        }
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = dfs(i, -1, children, k);
+        }
+        return res;
+    }
+
+    private int dfs(int node, int parent, List<List<Integer>> children, int k) {
+        if (k < 0) {
+            return 0;
+        }
+        int res = 1;
+        for (int child : children.get(node)) {
+            if (child == parent) {
+                continue;
+            }
+            res += dfs(child, node, children, k - 1);
+        }
+        return res;
+    }
+}
